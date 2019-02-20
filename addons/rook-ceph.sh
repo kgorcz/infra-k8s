532e2feedbfa229f58988df17d99c6feba20e85c
Kevin Gorczowski <kgorcz@gmail.com>
2019-02-20 00:31:33 +0000

    Wait for rgw pod before creating object store user

--- addons/rook-ceph.sh
+++ addons/rook-ceph.sh
@@ -8,11 +8,19 @@ cd cluster/examples/kubernetes/ceph/
 kubectl $kubecfg apply -f operator.yaml
 kubectl $kubecfg apply -f cluster.yaml
 kubectl $kubecfg apply -f object.yaml
+
+# Wait for the rgw pod before creating object user
+while [ $(kubectl $kubecfg -n rook-ceph get po | grep rook-ceph-rgw | grep Running | wc -l) -lt 1 ]
+do
+    echo "Waiting for rook-ceph-rgw pod..."
+    sleep 5
+done
+
 kubectl $kubecfg apply -f object-user.yaml
 
 while [ $(kubectl $kubecfg -n rook-ceph get secret rook-ceph-object-user-my-store-my-user | grep my-store-my-user | wc -l) -lt 1 ]
 do
-    echo "Waiting for rook-ceph-object-user-my-store-my-user..."
+    echo "Waiting for the object user secret..." 
     sleep 5
 done
 
