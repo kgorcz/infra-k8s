64b63d8290918c2cd6620007265347e3bf719ff8
Kevin Gorczowski <kgorcz@gmail.com>
2019-04-28 13:58:24 +0000

    Update contour external traffic policy to preserve original client IP

--- addons/contour.sh
+++ addons/contour.sh
@@ -10,5 +10,6 @@ sed "s|mycontour:5|quay.io/kgorcz/contour:1|" -i 02-contour.yaml
 sed "s|targetPort: 8080|targetPort: 8080\n   nodePort: 32323|" -i 02-service.yaml
 sed "s|targetPort: 8443|targetPort: 8443\n   nodePort: 32324|" -i 02-service.yaml
 sed "s|targetPort: 104|targetPort: 104\n   nodePort: 32325|" -i 02-service.yaml
+sed "s|type: NodePort|type: NodePort\n  externalTrafficPolicy: Local|" -i 02-service.yaml
 
 kubectl $kubecfg apply -f .
