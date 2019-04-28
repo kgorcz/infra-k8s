0c35392bd63b25880de95dac0658fe479274822e
Kevin Gorczowski <kgorcz@gmail.com>
2019-04-28 15:24:49 +0000

    Fix replica count

--- addons/contour.sh
+++ addons/contour.sh
@@ -11,6 +11,6 @@ sed "s|targetPort: 8080|targetPort: 8080\n   nodePort: 32323|" -i 02-service.yam
 sed "s|targetPort: 8443|targetPort: 8443\n   nodePort: 32324|" -i 02-service.yaml
 sed "s|targetPort: 104|targetPort: 104\n   nodePort: 32325|" -i 02-service.yaml
 sed "s|type: NodePort|type: NodePort\n externalTrafficPolicy: Local|" -i 02-service.yaml
-sed "s|replicas: 1|replicas: 4|" -i 02-service.yaml
+sed "s|replicas: 1|replicas: 4|" -i 02-contour.yaml
 
 kubectl $kubecfg apply -f .
