0316e9029968d02eb3539f5053ff0d2469e11165
Kevin Gorczowski <kgorcz@gmail.com>
2020-01-15 01:27:29 +0000

    Fix contour image reference

--- addons/contour.sh
+++ addons/contour.sh
@@ -7,7 +7,7 @@ git checkout heptio-integ
 cd deployment/deployment-grpc-v2
 
 sed "s|extensions/v1beta1|apps/v1|" -i 02-contour.yaml
-sed "s|mycontour:5|quay.io/kgorcz/contour:v0.8.0|" -i 02-contour.yaml
+sed "s|gcr.io/heptio-images/contour:v0.8.0|quay.io/kgorcz/contour:v0.8.0|" -i 02-contour.yaml
 sed "s|targetPort: 8080|targetPort: 8080\n   nodePort: 32323|" -i 02-service.yaml
 sed "s|targetPort: 8443|targetPort: 8443\n   nodePort: 32324|" -i 02-service.yaml
 sed "s|targetPort: 104|targetPort: 104\n   nodePort: 32325|" -i 02-service.yaml
